57eec0f413924c1e4ccb608224bde8e62763f73d
Minilin <a0966678326@gmail.com>
2022-07-07 15:09:22 +0800

    增加canvas動畫

---
+++ back/add_vote.php
@@ -0,0 +1,30 @@
+<form action="/vote/api/add_vote.php" method="post">
+    <div>
+        <label for="subject">投票主題：</label>
+        <input type="text" name="subject" id="subject">
+        <!-- 按新增選像觸發function more() -->
+        <input type="button" value="新增選項" onclick="more()">
+    </div>
+    <div id="selector">
+        <input type="radio" name="multiple" value="0" checked>
+        <label>單選</label>
+        <input type="radio" name="multiple" value="1" >
+        <label>複選</label>
+    </div>
+    <div id="options">
+        <div>
+            <label>選項:</label><input type="text" name="option[]">
+        </div>
+    </div>
+    <input type="submit" value="新增">
+
+</form>
+<script>
+    function more(){
+        let opt=`<div><label>選項:</label><input type="text" name="option[]"></div>`;
+                                        //  div的id
+        let opts=document.getElementById('options').innerHTML;
+        opts=opts+opt;
+        document.getElementById('options').innerHTML=opts;
+    }
+</script>
---
+++ layout/back_nav.php
@@ -0,0 +1,4 @@
+<nav>
+        <a href="back.php">投票列表</a>
+        <a href="?do=add_voie.php">新增投票</a>
+    </nav>
